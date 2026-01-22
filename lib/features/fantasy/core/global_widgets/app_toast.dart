7ab6a60bd2f9fa5427547ac7a74845ca44cbb4d5
Test User <test@example.com>
2026-01-22 12:23:26 +0530

    app tost conflict fix

--- lib/features/fantasy/core/global_widgets/app_toast.dart
+++ lib/features/fantasy/core/global_widgets/app_toast.dart
@@ -2,17 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:oktoast/oktoast.dart';
 import 'package:unified_dream247/features/fantasy/core/app_constants/app_colors.dart';
 
-void appToast(String msg, BuildContext context) {
-  showToast(
-    msg,
+ToastFuture appToast(String msg, BuildContext context) {
+  return showToast(
     position: ToastPosition.bottom,
     duration: const Duration(seconds: 2),
     backgroundColor: AppColors.lightCard,
-    textPadding: const EdgeInsets.all(10),
-    textStyle: const TextStyle(
-      color: AppColors.blackColor,
-      fontSize: 16,
-    ),
+    textPadding: const EdgeInsets.all(10.0),
+    msg,
+    textStyle: const TextStyle(color: AppColors.blackColor, fontSize: 16.0),
   );
 }
-
